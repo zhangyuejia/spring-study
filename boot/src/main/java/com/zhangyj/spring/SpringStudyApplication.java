fe863d5dc55e159c0c098e2ef0000b87a54f090f
zhangyuejia <1098922863@qq.com>
2022-05-15 22:34:14 +0800

    修改pom

--- boot/src/main/java/com/zhangyj/spring/SpringStudyApplication.java
+++ boot/src/main/java/com/zhangyj/spring/SpringStudyApplication.java
@@ -10,6 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringStudyApplication.class);
+        SpringApplication.run(SpringStudyApplication.class, args);
     }
 }
--- business/src/main/java/com/zhangyj/spring/business/asyncrsp/AsyncResponseController.java
+++ business/src/main/java/com/zhangyj/spring/business/asyncrsp/AsyncResponseController.java
@@ -1,8 +1,33 @@
 package com.zhangyj.spring.business.asyncrsp;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+
 /**
  * @author zhangyj
  */
+@RestController
 public class AsyncResponseController {
+
+    @GetMapping("/asyncResponse")
+    public Callable<String> getAsyncResponse(){
+        return () -> {
+            if(true){
+                throw new RuntimeException("此路不通");
+            }
+            return "你好";
+        };
+    }
+
+
+    @GetMapping("/syncResponse")
+    public String getSyncResponse(){
+        if(true){
+            throw new RuntimeException("此路不通");
+        }
+        return "你好1";
+    }
 }
