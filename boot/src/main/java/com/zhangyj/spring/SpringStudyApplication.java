eccceb3caeb91555326b0ea756014f6982466bc1
zhangyuejia <1098922863@qq.com>
2022-05-15 19:36:59 +0800

    构建项目

---
+++ boot/src/main/java/com/zhangyj/spring/SpringStudyApplication.java
@@ -0,0 +1,15 @@
+package com.zhangyj.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangyj
+ */
+@SpringBootApplication
+public class SpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyApplication.class);
+    }
+}
---
+++ business/src/main/java/com/zhangyj/spring/business/asyncrsp/AsyncResponseController.java
@@ -0,0 +1,8 @@
+package com.zhangyj.spring.business.asyncrsp;
+
+
+/**
+ * @author zhangyj
+ */
+public class AsyncResponseController {
+}
